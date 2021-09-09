c11801dc0134054d58213f93bda3b72354a7538f
harichandana1810 <52564706+harichandana1810@users.noreply.github.com>
2021-09-09 16:41:31 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GetService } from './services/get.service';
+import { ListComponent } from './components/list/list.component';
+import { DetailsComponent } from './components/details/details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CardComponent } from './components/card/card.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    DetailsComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  exports:[
+    CardComponent
+  ],
+  providers: [GetService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
